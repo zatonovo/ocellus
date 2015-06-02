9a2ff8a0737b3cf3656b1a7bee1ae5ad6c71ef0c
Brian Lee Yung Rowe <rowe@zatonovo.com>
2015-06-02 16:36:07 -0400

    Add logging

--- src/ocellus_stream_listener.erl
+++ src/ocellus_stream_listener.erl
@@ -19,20 +19,27 @@ handle_stream(Provider, RequestId) ->
 
     % stream closed
     {http, {RequestId, stream_end, _Headers}} ->
+      lager:info("[~p] Stream closed for ~p", [?MODULE,RequestId]),
       {ok, stream_end};
 
     % connected but received error cod
     % 401 unauthorised - authentication credentials rejected
     {http, {RequestId, {{_, 401, _}, _Headers, _Body}}} ->
+      Msg = "[~p] Authentication credentials rejected for ~p",
+      lager:error(Msg, [?MODULE,RequestId]),
       {error, unauthorised};
 
     % 406 not acceptable - invalid request to the search api
     {http, {RequestId, {{_, 406, _}, _Headers, Body}}} ->
+      Msg = "[~p] Invalid request for ~p",
+      lager:error(Msg, [?MODULE,RequestId]),
       {error, {invalid_params, Body}};
 
     % connection error
     % may happen while connecting or after connected
     {http, {RequestId, {error, Reason}}} ->
+      Msg = "[~p] Unexpected error for ~p: ~p",
+      lager:error(Msg, [?MODULE,RequestId,Reason]),
       {error, {http_error, Reason}};
 
     % message send by us to close the connection
